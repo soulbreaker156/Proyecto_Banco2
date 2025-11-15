5ec1324a9e2f3a17841f9672bc9f02c193800e88
soulbreaker156 <soulbreaker0220@gmail.com>
2025-11-14 19:14:43 -0600

    feat: creado el que se aumente el saldo del usuario y agregado estilos al formulrio de deposito y retiro

--- Models/TransaccionModel.php
+++ Models/TransaccionModel.php
@@ -1,14 +1,17 @@
 <?php
 
 require_once __DIR__ . '/../config/conexion.php';
+require_once __DIR__ . '/../Models/UsuarioModel.php';
 
 class TransaccionModel
 {
     private $conexion;
+    private $usuarioModel;
 
     public function __construct()
     {
         $this->conexion = getConexion();
+        $this->usuarioModel = new UsuarioModel();
     }
 
     public function procesarDeposito($monto, $concepto, $id_usuario)
@@ -18,6 +21,8 @@ class TransaccionModel
 
             $stmtInsertar = $this->conexion->prepare("INSERT INTO transacciones (monto,tipo_mov, concepto, fecha_mov,fk_usuario) VALUES (?, 'deposito', ?, NOW(), ?)");
             $resultado = $stmtInsertar->execute([$monto, $concepto, $id_usuario]);
+
+            $this->usuarioModel->actualizarSaldo($id_usuario, $monto);
 
             if (!$resultado) {
                 // Error al insertar
